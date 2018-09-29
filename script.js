8f2684d3e8c2a82d6a212f7b6e513613646a423d
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-09-29 14:37:21 +0200

    Add Stopwatch class and instance

---
+++ script.js
@@ -0,0 +1,10 @@
+class Stopwatch {
+  constructor(display) {
+    this.running = false;
+    this.display = display;
+    this.reset();
+    this.print(this.times);
+  }
+}
+
+const stopwatch = new Stopwatch(document.querySelector('.stopwatch'));
