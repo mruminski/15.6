7692a31457284f4c5864b8499ea0a8f983e7482c
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-09-29 15:21:51 +0200

    Add reset(), print(), format() and pad0()

--- js/script.js
+++ js/script.js
@@ -5,6 +5,30 @@ class Stopwatch {
     this.reset();
     this.print(this.times);
   }
+
+  reset() {
+    this.times = {
+      minutes: 0,
+      seconds: 0,
+      miliseconds: 0
+    };
+  }
+
+  print() {
+    this.display.innerText = this.format(this.times);
+  }
+
+  format(times) {
+    return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+  }
 }
 
-const stopwatch = new Stopwatch(document.querySelector('.stopwatch'));
+function pad0(val) {
+  let result = val.toString();
+  if (result.length < 2) {
+    result = '0' + result;
+  }
+  return result;
+}
+
+const stopwatch = new Stopwatch(document.querySelector(".stopwatch"));
